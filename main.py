5269dda3e2387afe2191e62594a59e39b53c899b
wajanfresh <89922092+wajanfresh@users.noreply.github.com>
2021-09-01 17:46:19 +0100

    Add files via upload

---
+++ main.py
@@ -0,0 +1,28 @@
+Question1 = input('what is your name? ')
+print('''
+      
+      ''')
+Question2 = input('how old are you? ')
+print('''
+      
+      ''')
+Question3 = input('which program do you attend? ')
+print('''
+      
+      ''')
+Question4 = input('which part of programming are you learning? ')
+print('''
+
+      ''')
+Question5 = input('has this class been interesting? ')
+print('''
+      
+      ''')
+Question6 = input('how has your NextGen Python Experiences been? ')
+
+print(f'''cool, {Question1} said that she is {Question2}
+she is attending {Question3}
+Not to talk about the fact that she is attending {Question4}
+And she said {Question5}
+And she said also {Question6}
+Thank you for answering our Questionnaire. Have a wonderful day!!!!''')
